41b658c7f0d52e8f0a58aa7f53a6064e62423c8d
Jeffrey Horn <jrhorn424@gmail.com>
2013-12-04 04:59:23 -0500

    Update configuration

--- home/.chruby.zsh
+++ home/.chruby.zsh
@@ -1,3 +1,2 @@
-[[ IS_MAC -eq 1 ]] && source /usr/local/share/chruby/chruby.sh
-[[ HAS_APT -eq 1 ]] && source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/chruby.sh
 chruby ruby-2.0
